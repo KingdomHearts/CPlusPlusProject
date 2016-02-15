6da08526e4d99f3e13b15e7fa9657bdbc9485989
RGravity <rogieroh@hotmail.com>
2016-02-15 16:44:01 +0100

    Raycast && Triangles Implemented in Mesh

--- mge_student_edition_v15.0.0/src/mge/core/Raycast.hpp
+++ mge_student_edition_v15.0.0/src/mge/core/Raycast.hpp
@@ -1,12 +1,21 @@
 #ifndef RAYCAST_H
 #define RAYCAST_H
 
+#include <glm.hpp>
+#include "Mesh.hpp"
+
 
 class Raycast
 {
     public:
-        Raycast();
-        virtual ~Raycast();
+        /**
+         * Check Ray Intersection of a Triangle
+        */
+        static bool Ray_Intersect_Triangle(glm::vec3 pStartPoint, glm::vec3 pSlopeLine, glm::vec3 pVertice0, glm::vec3 pVertice1, glm::vec3 pVertice2, bool pExtend_To_Infinity, bool pCull_Backfaces);
+        /**
+         * Go through all the triangles of a Mesh and check Ray Intersection of each Triangle
+         */
+        static bool Ray_Intersect_Mesh(Mesh* pMesh, glm::vec3 pStartPoint, glm::vec3 pSlopeLine, bool pExtend_To_Infinity, bool pCull_Backfaces);
     protected:
     private:
 };

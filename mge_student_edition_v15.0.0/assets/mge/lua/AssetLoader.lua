da1586766587c1b0db6c18ed9b8fd37fd316242e
wargamer88 <m.van.denzel@home.nl>
2016-02-18 15:20:16 +0100

    added gitignore again and updated lualoader
    lua loader works with no textures selected

--- mge_student_edition_v15.0.0/assets/mge/lua/AssetLoader.lua
+++ mge_student_edition_v15.0.0/assets/mge/lua/AssetLoader.lua
@@ -1,3 +1,3 @@
-AddModel('teapot_flat','teapot_flat.obj','bricks.png','1.64,-0.63,-1.92','231.7649')
+AddModel('teapot_flat','teapot_flat.obj','','1.64,-0.63,-1.92','231.7649')
 AddModel('mockup','mockup.obj','land.png','0,-1.92,0','0')
 AddModel('suzanna_flat','suzanna_flat.obj','bricks.png','0.34,0,0','0')

eeabab85e1e30e240af139111fb61ff3c0400b5d
wargamer88 <m.van.denzel@home.nl>
2016-02-18 11:25:19 +0100

    UnityLuaGen

---
+++ UnityLuaGenerator/UnityLuaGenerator/Assets/Script/ObjectsToLuaScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectsToLuaScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

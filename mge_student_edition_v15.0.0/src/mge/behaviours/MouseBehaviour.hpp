e80f24275d3e27e7ab09cdce963ebcbee9844a14
wargamer88 <m.van.denzel@home.nl>
2016-03-14 15:07:33 +0100

    mouse stuff

--- mge_student_edition_v15.0.0/src/mge/behaviours/MouseBehaviour.hpp
+++ mge_student_edition_v15.0.0/src/mge/behaviours/MouseBehaviour.hpp
@@ -35,6 +35,7 @@ class MouseBehaviour : public AbstractBehaviour
         GameObject* _inventoryBox;
         GameObject* _progressBar;
         GameObject* _scrollers;
+        glm::vec3 _scrollerPosition = glm::vec3(-0.462, 0.243, -0.9);
 
         // position
         glm::vec3 _position = glm::vec3( 0, 3, 10 );
